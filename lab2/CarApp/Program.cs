a9298f6f2a8587e46ff93c966b96cf936da377b6
Witold Hajkowicz <witold.hajkowicz@student.put.poznan.pl>
2024-10-23 11:26:33 +0200

    Add lab2

--- lab2/CarApp/Program.cs
+++ lab2/CarApp/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace CarApp;
+﻿using System.Reflection;
+using System.Configuration;
+
+namespace CarApp;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Interfaces.IDAO dao = new DAOMock1.DAOMock1();
+        //foreach (var arg in args) {
+        //    Console.WriteLine(arg);
+        //}
+        string libraryName = ConfigurationManager.AppSettings["library_name"];
+
+        Assembly assembly = Assembly.UnsafeLoadFrom(libraryName);
+        Type typeToCreate = null;
+        foreach (Type type in assembly.GetTypes()) {
+            if ( type.IsAssignableTo(typeof(Interfaces.IDAO))) {
+                typeToCreate = type;
+                break;
+            }
+        }
+
+        //Interfaces.IDAO dao = new DAOMock1.DAOMock1();
+        Interfaces.IDAO dao = Activator.CreateInstance(typeToCreate) as Interfaces.IDAO;
+        //Interfaces.IDAO dao2 = new DAOMock2.DAOMock2();
 
         Console.WriteLine("## Producers ##");
         foreach(Interfaces.IProducer p in dao.GetAllProducers()){
@@ -15,6 +34,17 @@ internal class Program
         foreach(Interfaces.ICar c in dao.GetAllCars()){
             Console.WriteLine(c);
         }
+
+
+        /*Console.WriteLine("## Producers ##");
+        foreach(Interfaces.IProducer p in dao2.GetAllProducers()){
+            Console.WriteLine(p);
+        }
+
+        Console.WriteLine("## Cars ##");
+        foreach(Interfaces.ICar c in dao2.GetAllCars()){
+            Console.WriteLine(c);
+        }*/
         
     }
 }
